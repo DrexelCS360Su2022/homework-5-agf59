cece0bdbe2a4343c0a7493019f4543c054f16279
Aaron Fuhrmann <agf59@tux5.cci.drexel.edu>
2022-08-05 20:13:07 -0400

    finished all of problem 1 and most of problem 2

--- HaskellIntro.hs
+++ HaskellIntro.hs
@@ -15,35 +15,51 @@ isThisWorking = "Yes"
 --
 
 lastDigit :: Integer -> Integer
-lastDigit = error "lastDigit not yet defined"
+lastDigit x = if x < 10 then x else x `mod` 10
 
 dropLastDigit :: Integer -> Integer
-dropLastDigit = error "dropLastDigit not yet defined"
+dropLastDigit x = if x < 10 then 0 else x `div` 10
 
 toDigits :: Integer -> [Integer]
-toDigits = error "toDigits not yet defined"
+toDigits x = if x <= 0 then [] else toDigits (dropLastDigit x) ++ [lastDigit x] --append list versions of the digits to each other instead of cons since it lets us do things in a right to left order
+
+doubleEveryOtherH :: [Integer] -> [Integer]
+doubleEveryOtherH [] = [] --if list is empty just return it
+doubleEveryOtherH [x] = [x] --if list is a single element there's nothing to check out
+doubleEveryOtherH (x:x2:xs) = x : (2 * x2 : (doubleEveryOtherH xs)) --isolate the first 2 elements of the list, then double the second, and finally perform the same operation on the rest of the list
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = error "doubleEveryOther not yet defined"
+doubleEveryOther xs = reverse (doubleEveryOtherH (reverse xs)) --use helper to do operation on a reversed version of the list
+
+sumDigsH :: [Integer] -> Integer --helper for sumDigits
+sumDigsH xs = foldl (+) 0 xs
 
 sumDigits :: [Integer] -> Integer
-sumDigits = error "sumDigits not yet defined"
+sumDigits xs = foldl (+) 0 (map sumDigsH (map toDigits xs)) --hacky use of helper functions. Digiconverters all elements to become lists of single digit elements, uses map to add those up, then folds the list in to add those subtotals
 
+
+--Assumes check digit is the final digit. Drops it in the calculations, but revisits it for the final comparison
+--Double the value of every second digit from the right after dropping final digit. Uses doubleEveryOther
+--sum the digits of this new list. Uses sumDigits.
+--the check digit should equal to this sum mod 10
 validate :: Integer -> Bool
-validate = error "validate not yet defined"
+validate x = (sumDigits (doubleEveryOther (toDigits x)) `mod` 10) == 0
 
 --
 -- Problem 2
 --
 
 pow :: (a -> a) -> Int -> a -> a
-pow = error "pow not yet defined"
+pow _ 0 = id 
+pow f n = f . (pow f (n-1))
 
 g :: Integer -> Integer
-g = error "g not yet defined"
+g 0 = 0
+g n = n - ((pow g 2) (n-1))
 
 h :: Integer -> Integer
-h = error "h not yet defined"
+h 0 = 0
+h n = n - ((pow h 3) (n-1))
 
 d :: Int -> Integer -> Integer
 d = error "d not yet defined"
--- README.md
+++ README.md
@@ -10,8 +10,15 @@ Put anything else you want us to know here.
 
 Please let us know how much time you spent on each problem. You may enter time using any format described [here](https://github.com/wroberts/pytimeparse). Please put your time estimates *on the same line* as the problem label, not on the following line.
 
-Problem 1:  
+Problem 1: 107m
+1.1 was very easy, just mod and div.
+1.2 was a bit harder, but I figured out a simple way of doing things by just appending the last digit as a list instead of consing it, since that solved the typing issue between Integer and [Integer]
+1.3 was harder still, and I had to apply helper functions as suggested in addition to reviewing lecture notes about splitting functions into elements cons'd to other elements
+1.4 was also still harder, and I had to get a bit creative on my algorithm for it. Still nothing that made me pull my hair out
+1.5 was pretty easy, but I ran into issues where I wasn't getting the correct answer from the examples on the homework pdf. Clarification received from TA 
 
-Problem 2:  
+Problem 2: 38m
+pow, g, and h were all very easy to implement as their pseudocode was basically described in perfectly clear terms from the outset.
+d was a bit more tricky, if only because I was struggling to understand the logic behind it.
 
 Problem 3:  
